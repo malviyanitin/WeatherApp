dbb1786fa78a187df15fb436dee28fd10ea43c9c
Nitin Malviya <nitinmalvi478@gmail.com>
2023-05-16 06:08:46 +0530

    worked on Looks + About button.

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const APIkey = '22764bf47f66e4ce9ba7ec70be72d1a2';
 let k;
-async function fetchData(city) {
-     try {
-      let response = await fetch(url+city + '&appid=' + APIkey);
-     let result = await response.json();
-      console.log(result);
-      console.log(result.humidity);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+
+  
   async function fetchData(city) {
     try {
       let response = await fetch(url+city + '&appid=' + APIkey);
@@ -23,27 +15,27 @@ async function fetchData(city) {
  
   fetchData("delhi").then(result => {
     console.log(result);
-  dltemp.innerHTML       = (result.main.temp-273).toFixed(2);
-  dlfeels_like.innerHTML = (result.main.feels_like-273).toFixed(2);
+  dltemp.innerHTML       = (result.main.temp-273).toFixed(1);
+  dlfeels_like.innerHTML = (result.main.feels_like-273).toFixed(1);
   dlhumidity.innerHTML   = result.main.humidity;
-  dlmin_temp.innerHTML   = (result.main.temp_min-273).toFixed(2);
-  dlmax_temp.innerHTML   = (result.main.temp_max-273).toFixed(2);
+  dlmin_temp.innerHTML   = (result.main.temp_min-273).toFixed(1);
+  dlmax_temp.innerHTML   = (result.main.temp_max-273).toFixed(1);
   });
   fetchData("mumbai").then(result => {
     console.log(result);
-    mtemp.innerHTML =        (result.main.temp-273).toFixed(2);
-    mfeels_like.innerHTML =  (result.main.feels_like-273).toFixed(2);
+    mtemp.innerHTML =        (result.main.temp-273).toFixed(1);
+    mfeels_like.innerHTML =  (result.main.feels_like-273).toFixed(1);
     mhumidity.innerHTML =  result.main.humidity;
-    mmin_temp.innerHTML =  (result.main.temp_min-273).toFixed(2);
-    mmax_temp.innerHTML =  (result.main.temp_max-273).toFixed(2);
+    mmin_temp.innerHTML =  (result.main.temp_min-273).toFixed(1);
+    mmax_temp.innerHTML =  (result.main.temp_max-273).toFixed(1);
     });
     fetchData("chennai").then(result => {
       console.log(result);
-      ctemp.innerHTML        = (result.main.temp-273).toFixed(2);
-      cfeels_like.innerHTML  = (result.main.feels_like-273).toFixed(2);
+      ctemp.innerHTML        = (result.main.temp-273).toFixed(1);
+      cfeels_like.innerHTML  = (result.main.feels_like-273).toFixed(1);
       chumidity.innerHTML    = result.main.humidity;
-      cmin_temp.innerHTML    = (result.main.temp_min-273).toFixed(2);
-      cmax_temp.innerHTML    = (result.main.temp_max-273).toFixed(2);
+      cmin_temp.innerHTML    = (result.main.temp_min-273).toFixed(1);
+      cmax_temp.innerHTML    = (result.main.temp_max-273).toFixed(1);
       });
    submit.addEventListener("click",(e)=>{
     console.log(inputt.vaue);
@@ -51,14 +43,14 @@ async function fetchData(city) {
     fetchData(inputt.value).then(result => {
       console.log(result);
       searchCard.innerHTML= inputt.value;
-      ctemp.innerHTML       = (result.main.temp-273).toFixed(2);
-      cfeels_like.innerHTML = (result.main.feels_like-273).toFixed(2);
+      ctemp.innerHTML       = (result.main.temp-273).toFixed(1);
+      cfeels_like.innerHTML = (result.main.feels_like-273).toFixed(1);
       chumidity.innerHTML   = result.main.humidity;
-      cmin_temp.innerHTML   = (result.main.temp_min-273).toFixed(2);
-      cmax_temp.innerHTML   = (result.main.temp_max-273).toFixed(2);
+      cmin_temp.innerHTML   = (result.main.temp_min-273).toFixed(1);
+      cmax_temp.innerHTML   = (result.main.temp_max-273).toFixed(1);
       });
    })
   
-  
+
   
   
